94229cd36dd74db986c620b5ee0d15802c298d43
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-09 16:23:46 -0800

    introduced different product code support for bitflyer exchange

--- Api_analyzer.py
+++ Api_analyzer.py
@@ -6,6 +6,10 @@ from urllib.request import urlopen
 api_urls = {    'ticker' : "http://api.bitflyer.jp/v1/ticker",
                 'get_ticker' : "http://api.bitflyer.jp/v1/getticker"}
 
+product_codes = ["BTC_JPY", "FX_BTC_JPY", "ETH_BTC"]
+get_query = "?product_code="
+
+
 # Accepts a string for the URL of the API 
 # Returns a json reponse in the format of a dictionary
 def query_api(api):
@@ -14,13 +18,15 @@ def query_api(api):
     return response_json
 
 
-def query_ticker():
-    return query_api(api_urls['ticker'])
+def query_ticker(product_code="FX_BTC_JPY"):
+    api = api_urls['ticker'] + get_query + product_code
+    return query_api(api)
 
-def query_get_ticker():
+def query_get_ticker(product_code="FX_BTC_JPY"):
+    api = api_urls['get_ticker'] + get_query + product_code
     return query_api(api_urls['get_ticker'])
 
-''' #Sample code to test functionality of query 
+#Sample code to test functionality of query 
 while(True):
     print(query_ticker())
-'''    
+    
