2fab1433cdba528e2894dfd33551a6af2296f188
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-26 12:18:49 -0700

    removed spaces

--- agent_testEnv.py
+++ agent_testEnv.py
@@ -1,10 +1,6 @@
 import csv, sys
 
-
-
 filename = 'bfx_2017-03-25.csv'
-
-
 
 f = open(filename, newline='')
 test_environment = csv.reader(f)
@@ -16,8 +12,6 @@ try:
         #EACH STATE REPRESENTS A 2-5 SECONDS OF ELAPSED TIME
         print(state) # state is an array
         print("last_price: " + state[0]) # testing array output
-
-
 
 except csv.Error as e:
     sys.exit('file {}, line {}: {}'.format(filename, test_environment.line_num, e))
