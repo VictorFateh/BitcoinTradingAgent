64e6d4c8a70798f72c596be8676fc31b3a722745
Alexis Candelaria <alexiscandelaria1@gmail.com>
2017-03-26 12:41:30 -0700

    name change from environment to percept

--- agent_testEnv.py
+++ agent_testEnv.py
@@ -3,15 +3,15 @@ import csv, sys
 filename = 'bfx_2017-03-25.csv'
 
 f = open(filename, newline='')
-test_environment = csv.reader(f)
+percepts = csv.reader(f)
 
 try:
-    for state in test_environment:
+    for data in percepts:
 
-        #TODO AGENT WILL TAKE A ROW INPUT FROM THE ENVIRONMENT AS ITS PERCEPT
+        #TODO AGENT WILL TAKE A ROW INPUT FROM PERCEPT
         #EACH STATE REPRESENTS A 2-5 SECONDS OF ELAPSED TIME
-        print(state) # state is an array
-        print("last_price: " + state[0]) # testing array output
+        print(data) # state is an array
+        print("last_price: " + data[0]) # testing array output
 
 except csv.Error as e:
-    sys.exit('file {}, line {}: {}'.format(filename, test_environment.line_num, e))
+    sys.exit('file {}, line {}: {}'.format(filename, percepts.line_num, e))
